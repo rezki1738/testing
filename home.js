3da20b038aa8472fbc4f77f10def79dfc0f8d892
rezki1738 <133694127+rezki1738@users.noreply.github.com>
2023-05-16 01:36:43 +0200

    testing if this work

---
+++ home.js
@@ -0,0 +1,14 @@
+const track = document.getElementById("image-track");
+window.onmousedown = e => {
+  track.dataset.mouseDownAt =   e.clientX;
+
+}
+
+window.onmousemove  = e => {
+if(track.dataset.mouseDownAt==="0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+    maxDelta = window.innerWidth /2;
+    const percentage = (mouseDelta / maxDelta) *100;
+    track.style.transform = 'translate(${percentage}%,-50%)';
+}
